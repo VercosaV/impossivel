12a3c17b8da153497cc659fd474fa32af50f94c3
VercosaV <victorhvercosag@gmail.com>
2026-08-19 16:47:19 -0300

    bora

--- config/logging.php
+++ config/logging.php
@@ -60,7 +60,10 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => env('VERCEL') ? '/tmp/laravel.log' : storage_path('logs/laravel.log'),
+            'path' => (function () {
+            $logPath = storage_path('logs/laravel.log');
+            return is_writable(dirname($logPath)) ? $logPath : '/tmp/laravel.log';
+            })(),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
         ],
