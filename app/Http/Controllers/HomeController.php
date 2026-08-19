5a9f3d9b05067ebe96867ff625f84be1b3ede472
VercosaV <victorhvercosag@gmail.com>
2026-08-19 17:25:13 -0300

    agora bora fazer o impossível

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function welcome(Request $request)
+    {
+        return redirect()->route('welcome')->with('sucesso', 'Deplou concluído!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+*/
+
+Route::get('/', [PageController::class, 'welcome'])->name('welcome');
